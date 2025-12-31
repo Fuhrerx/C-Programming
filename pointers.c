cb39ec760fe6ab1e886269b65a8810bee3d70ef7
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2025-12-31 20:23:51 +0530

    pointers

---
+++ pointers.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+    // declaration of a pointer
+
+    int *ptr;
+
+    // assign a normal variable to a pointers
+
+    int a = 27;
+
+    ptr = &a;
+
+    // reference and dereference operators
+
+    int age = 10;
+    int *ageptr = &age;
+
+    printf("%d\n", age);
+    printf("%d\n", &age);
+    printf("%d\n", *&age);
+
+    // pointer arethemetic
+
+    int no = 10;
+    int *noptr = &no;
+
+    printf("pointers original address : %u\n", noptr);
+
+    noptr = noptr + 1;
+
+    printf("pointers address after incriment : %u\n", noptr);
+
+    noptr = noptr - 1;
+
+    printf("pointers address after decriment : %u\n", noptr);
+
+    noptr = noptr + 3;
+
+    printf("pointers address after add : %u\n", noptr);
+
+    noptr = noptr - 3;
+
+    printf("pointers address after sub : %u\n", noptr);
+
+    return 0;
+}
