415c7e9ab6ef4e036a62ad2db3fc926a8e5c563f
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2025-12-31 19:46:09 +0530

    functions

---
+++ funcs.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+// function declaration (prototype)
+
+void display();
+void add(int x, int y); // int x and int y are parameters
+int add1(int x, int y);
+
+// main function
+
+int main()
+{
+
+    add(10, 20); // 10,20 are arguments
+    display();
+    add1(100, 100);
+    display();
+
+    return 0;
+}
+
+// display function with no parameters
+
+void display()
+{
+    printf("end of funtion output\n");
+}
+
+// add function with no return type
+
+void add(int x, int y)
+{
+    int result = x + y; // local variable
+    printf("sum of %d and %d is : %d\n", x, y, result);
+}
+
+// add function with return type
+
+int add1(int x, int y)
+{
+    int result = x + y; // local variable
+    return printf("sum of %d and %d is : %d\n", x, y, result);
+}
