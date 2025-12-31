c7acdb2e8d8862bcfa24e1ac96f3c13635820d71
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2025-12-31 17:26:54 +0530

    switch case

---
+++ switch.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char active[4];
+    char modified[4];
+    int isActive;
+
+    printf("is the user still active? (yes/no) : ");
+    scanf("%s", &active);
+
+    for (int i = 0; active[i] != '\0'; i++)
+    {
+        modified[i] = tolower(active[i]);
+    }
+
+    if (modified == "yes")
+    {
+        isActive == 1;
+    }
+    else
+    {
+        isActive == 0;
+    }
+
+    if (modified == "no")
+    {
+        isActive = 0;
+    }
+    else
+    {
+        isActive == 1;
+    }
+
+    switch (isActive)
+    {
+    case 1:
+        printf("the user is active");
+        break;
+
+    case 0:
+        printf("the user is inactive");
+        break;
+
+    default:
+        printf("invalid");
+    }
+    return 0;
+}
