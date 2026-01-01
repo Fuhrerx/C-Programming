905785571cbb05461fef4d95e0523da6768c766b
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-01-01 18:04:10 +0530

    structures

---
+++ structs.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+// declaration and definition of structures *(singular)
+
+typedef struct
+{
+    char name[20];
+    char author[25];
+    float price;
+
+} books;
+
+// declaration and definition of structures *(array)
+
+typedef struct
+{
+    char items[25];
+    char desc[50];
+    float mrp;
+
+} list;
+
+// global struct declaration
+
+books b1;
+list l[2];
+
+void display_books();
+void display_list();
+
+int main(int argc, char *argv[])
+{
+
+    printf("name :");
+    scanf("%s", b1.name);
+
+    printf("author :");
+    scanf("%s", b1.author);
+
+    printf("price :");
+    scanf("%f", &b1.price);
+
+    printf("\nbooks list\n");
+    display_books();
+
+    for (int i = 0; i < 2; i++)
+    {
+        printf("name of the item :");
+        scanf("%s", l[i].items);
+
+        printf("description :");
+        scanf("%s", l[i].desc);
+
+        printf("MRP :");
+        scanf("%f", &l[i].mrp);
+    }
+
+    printf("\nlist of items brought\n");
+    display_list();
+
+    return 0;
+}
+
+void display_books()
+{
+    printf("name of the book : %s\n", b1.name);
+
+    printf("author of the book : %s\n", b1.author);
+
+    printf("price of the book : %f\n", b1.price);
+
+    printf("\n");
+}
+
+void display_list()
+{
+    for (int i = 0; i < 2; i++)
+    {
+
+        printf("items brought : %s\n", l[i].items);
+        printf("description : %s\n", l[i].desc);
+        printf("price of the item : %f\n", l[i].mrp);
+        printf("\n");
+    }
+}
